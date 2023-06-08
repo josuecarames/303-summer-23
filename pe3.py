5a84bad638d37e98284b51fd5d9f68053d6fe284
josuecarames <josue.carames@icloud.com>
2023-06-08 11:59:55 -0600

    improved the readability of the code

--- pe3.py
+++ pe3.py
@@ -1,34 +1,35 @@
-
-
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-
 input_text = input("Enter the text you want to encode: ")
-
 shift = int(input("Please enter the shift you want to use to encode the entered text: "))
 
 def encode(input_text, shift):
     encoded_string = ""
-    for i in input_text:
-        encoded_text = alphabet[alphabet.find(i) + shift - 1] 
-        encoded_string += encoded_text
+    for char in input_text:
+        if char in alphabet:
+            encoded_index = (alphabet.find(char) + shift - 1) % 26
+            encoded_char = alphabet[encoded_index]
+            encoded_string += encoded_char
+        else:
+            encoded_string += char
     return encoded_string
-    print("The encoded text is: " + encoded_string)
-
-encoded_text = encode(input_text, shift)
-
-print(encoded_text)
 
 def decode(encoded_string, shift):
     decoded_string = ""
-    if encoded_string is False:
-        encoded_string = input("Enter the text you want to decode: ")
-    else:
-        for i in encoded_string:
-            decoded_text = alphabet[alphabet.find(i) - shift + 1]
-            decoded_string += decoded_text
-        return decoded_string
-    print("The decoded text is: " + decoded_string)
+    for char in encoded_string:
+        if char in alphabet:
+            decoded_index = (alphabet.find(char) - shift + 1) % 26
+            decoded_char = alphabet[decoded_index]
+            decoded_string += decoded_char
+        else:
+            decoded_string += char
+    return decoded_string
+
+encoded_text = encode(input_text, shift)
+
+print("The encoded text is:", encoded_text)
 
 decoded_text = decode(encoded_text, shift)
 
-print(decoded_text)
+print("The decoded text is:", decoded_text)
+
+
