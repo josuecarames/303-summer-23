d724a1bf19a7c60352a6992d1faa67997c0e6455
josuecarames <josue.carames@icloud.com>
2023-06-08 18:31:54 -0600

    BankAccount working, yet to verify Checking and Saving

--- pe3.py
+++ pe3.py
@@ -1,78 +1,63 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-input_text = input("Enter the text you want to encode: ")
-shift = int(input("Please enter the shift you want to use to encode the entered text: "))
+# alphabet = "abcdefghijklmnopqrstuvwxyz"
+# input_text = input("Enter the text you want to encode: ")
+# shift = int(input("Please enter the shift you want to use to encode the entered text: "))
 
-def encode(input_text, shift):
-    encoded_string = ""
-    for char in input_text:
-        if char in alphabet:
-            encoded_index = (alphabet.find(char) + shift - 1) % 26
-            encoded_char = alphabet[encoded_index]
-            encoded_string += encoded_char
-        else:
-            encoded_string += char
-    return encoded_string
+# def encode(input_text, shift):
+#     encoded_string = ""
+#     for char in input_text:
+#         if char in alphabet:
+#             encoded_index = (alphabet.find(char) + shift - 1) % 26
+#             encoded_char = alphabet[encoded_index]
+#             encoded_string += encoded_char
+#         else:
+#             encoded_string += char
+#     return encoded_string
 
-def decode(encoded_string, shift):
-    decoded_string = ""
-    for char in encoded_string:
-        if char in alphabet:
-            decoded_index = (alphabet.find(char) - shift + 1) % 26
-            decoded_char = alphabet[decoded_index]
-            decoded_string += decoded_char
-        else:
-            decoded_string += char
-    return decoded_string
+# def decode(encoded_string, shift):
+#     decoded_string = ""
+#     for char in encoded_string:
+#         if char in alphabet:
+#             decoded_index = (alphabet.find(char) - shift + 1) % 26
+#             decoded_char = alphabet[decoded_index]
+#             decoded_string += decoded_char
+#         else:
+#             decoded_string += char
+#     return decoded_string
 
-encoded_text = encode(input_text, shift)
+# encoded_text = encode(input_text, shift)
 
-print("The list of letters encoded are: " + str(tuple(encoded_text)) + " The encoded text is: " + str(encoded_text))
+# print("The list of letters encoded are: " + str(tuple(encoded_text)) + " The encoded text is: " + str(encoded_text))
 
-decoded_text = decode(encoded_text, shift)
+# decoded_text = decode(encoded_text, shift)
 
-print("The list of letters decoded are: " + str(tuple(decoded_text)) + " The encoded text is: " + str(decoded_text))
+# print("The list of letters decoded are: " + str(tuple(decoded_text)) + " The encoded text is: " + str(decoded_text))
 
 
 #2 Classes
 
+# # Import modules
+
 import datetime
 from datetime import date
-from dateutil.relativedelta import relativedelta
-
-# Create a class called BankAccount.
+# from dateutil.relativedelta import relativedelta
 
 class BankAccount:
+    def __init__(self, name="Clocks", ID="123", creation_date=date.today(), balance=0):
+        self.name = name
+        self.ID = ID
+        self.creation_date = creation_date
+        self.balance = balance
+        if self.creation_date > date.today():
+            raise Exception("Invalid creation date")
 
-# a) The class is initialized with the owner’s name (name), alphanumeric ID (ID), date of creation
-# (creation date), and balance. Ensure that creation date is of type datetime.date. The date
-# of creation can be the current date or a past date, but not a date in the future. If a creation
-# date in the future is supplied as the creation date value, the program must raise an exception
-# of class Exception.
+    def deposit(self, amount):
+        self.balance += amount
 
-    def __init__(self, name, ID, creation_date, balance):
-        name = self.name
-        ID = self.ID
-        creation_date = self.creation_date
-        balance = self.balance
+    def withdraw(self, amount):
+        self.balance -= amount
 
-    def func():
-        creation_date = datetime.date(creation_date)
-        if creation_date > datetime.date.today():
-            raise ValueError("The creation_date cannot have a future date and time")
-        else:
-            #TBD
-    
-    Exception
-
-
-
-# b) It has 3 instance methods: deposit(amount), withdraw(amount), and view balance().
-
-deposit_amount = deposit(self, amount)
-
-withdrawal_amount = withdraw(self, amount)
-
-view_balance = view_balance(self)
+    def view_balance(self):
+        return self.balance
 
 # c) There are two subclasses to BankAccount: a SavingsAccount and a CheckingAccount.
 # • SavingsAccount: only allows withdrawals after 6 months has passed since the creation of
@@ -82,32 +67,29 @@ view_balance = view_balance(self)
 # results in a negative balance.
 
 class SavingsAccount(BankAccount):
-    def __init__(self):
-        super().__init__()
-        # self.TBD
+    # def __init__(self):
+    #     super().__init__()
 
-    # def allow_withdrawal():
-    #   date_difference = relativedelta(date(creation_date), datetime.date.today())  
-    #   if date_difference.months >= 6 or date_difference.year > 0:
-        #     TBD
-        # else:
-        #     TBD
-
-
+    def withdraw(self, amount):
+        date_difference = relativedelta(date(creation_date), datetime.date.today())  
+        if date_difference.months >= 6 or date_difference.year > 0:
+            super().withdraw(amount)
+        else:
+            print("You cannot make a withdrawal before your account has reached 6 months maturity")
 
 class CheckingAccount(BankAccount):
-    def __init__(self):
-        super().__init__()
-        # self.TBD
+    # def __init__(self):
+    #     super().__init__()
 
-    def 
+    def withdraw(self, amount):
+        if self.balance - amount < 0:
+            self.balance -= (amount + 30)
+        else:
+            super().withdraw(amount)
 
-# d) Make sure your names are precisely consistent with the following:
-# • Class names - BankAccount, CheckingAccount, SavingsAccount
 # • Method Names - deposit(self, amount), withdraw(self, amount), view_balance(self)
 
 
+test = BankAccount(name="test", ID="321", creation_date=date(), balance=10)
 
-# e) The BankAccount class should have default values for all the arguments as follows: name:
-# ”Clocks”, id: ”123”, creation date: date.today(), balance: 0.
-# • The creation date should be of type datetime.date
+print(test.name, test.ID, test.creation_date, test.balance)
