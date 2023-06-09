71ea9c3faa4fcf2b7eb3bfabb5c2ed3618c4e91b
josuecarames <josue.carames@icloud.com>
2023-06-09 16:00:35 -0600

    tested the classes and worked. Also, I converted the input_text in Functions to lowercase to avoid an exception

--- pe3.py
+++ pe3.py
@@ -1,14 +1,14 @@
 # 1. Functions
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-input_text = input("Enter the text you want to encode: ")
+input_text = input("Enter the text you want to encode: ").lower()
 shift = int(input("Please enter the shift you want to use to encode the entered text: "))
 
 def encode(input_text, shift):
     encoded_string = ""
     for char in input_text:
         if char in alphabet:
-            encoded_index = (alphabet.find(char) + shift - 1) % 26
+            encoded_index = (alphabet.find(char) + shift) % 26
             encoded_char = alphabet[encoded_index]
             encoded_string += encoded_char
         else:
@@ -19,7 +19,7 @@ def decode(encoded_string, shift):
     decoded_string = ""
     for char in encoded_string:
         if char in alphabet:
-            decoded_index = (alphabet.find(char) - shift + 1) % 26
+            decoded_index = (alphabet.find(char) - shift) % 26
             decoded_char = alphabet[decoded_index]
             decoded_string += decoded_char
         else:
@@ -32,25 +32,27 @@ print("The list of letters encoded are: " + str(tuple(encoded_text)) + " The enc
 
 decoded_text = decode(encoded_text, shift)
 
-print("The list of letters decoded are: " + str(tuple(decoded_text)) + " The encoded text is: " + str(decoded_text))
+print("The list of letters decoded are: " + str(tuple(decoded_text)) + " The decoded text is: " + str(decoded_text))
 
+
+# Review why the first letter is not encoding
 
 #2 Classes
 
 # # Import modules
 
 import datetime
-from datetime import date
-# from dateutil.relativedelta import relativedelta
+from datetime import date, timedelta
 
 class BankAccount:
-    def __init__(self, name="Clocks", ID="123", creation_date=date.today(), balance=0):
+    def __init__(self, name="Clocks", ID="123", creation_date=datetime.date.today(), balance=0):
         self.name = name
         self.ID = ID
         self.creation_date = creation_date
         self.balance = balance
         if self.creation_date > date.today():
-            raise Exception("Invalid creation date")
+            raise Exception("creation_date cannot be in the future")
+        self.maturity_date = self.creation_date + timedelta(days=180)
 
     def deposit(self, amount):
         self.balance += amount
@@ -62,19 +64,15 @@ class BankAccount:
         return self.balance
 
 class SavingsAccount(BankAccount):
-    # def __init__(self):
-    #     super().__init__()
 
     def withdraw(self, amount):
-        date_difference = relativedelta(date(creation_date), datetime.date.today())  
-        if date_difference.months >= 6 or date_difference.year > 0:
+        current_date = datetime.date.today()  
+        if current_date >= self.maturity_date:
             super().withdraw(amount)
         else:
             print("You cannot make a withdrawal before your account has reached 6 months maturity")
 
 class CheckingAccount(BankAccount):
-    # def __init__(self):
-    #     super().__init__()
 
     def withdraw(self, amount):
         if self.balance - amount < 0:
@@ -83,8 +81,28 @@ class CheckingAccount(BankAccount):
             super().withdraw(amount)
 
 
+# test = BankAccount(name="test", ID="321", creation_date=date(2023, 6, 9), balance=0) # BankAccount class works
 
+# test = BankAccount(name="test", ID="321", creation_date=date(2023, 9, 9), balance=0) # Future date raises Exception
 
-test = BankAccount(name="test", ID="321", creation_date=date(), balance=10)
+# test.withdraw(25)
 
-print(test.name, test.ID, test.creation_date, test.balance)
+# print("Updated balance: ", test.view_balance())
+
+# test = CheckingAccount(name="test", ID="321", creation_date=date(2023, 6, 9), balance=0) # Overdraft +$30
+
+# test = SavingsAccount(name="test", ID="321", creation_date=datetime.date(2023, 1, 1), balance=100) # Account maturity older than 6 months 
+
+# print(test.name, test.ID, test.creation_date, test.balance)
+
+# test.withdraw(25)
+
+# print("Updated balance: ", test.view_balance())
+
+# test = SavingsAccount(name="test", ID="321", creation_date=datetime.date.today(), balance=100) # Account maturity younger than 6 months doesn't withdraw from balance
+
+# print(test.name, test.ID, test.creation_date, test.balance)
+
+# test.withdraw(25)
+
+# print("Updated balance: ", test.view_balance())
