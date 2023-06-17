4331d1cfb5039d799d127e2ce096eea89c0007db
Josue Carames <35548140+josuecarames@users.noreply.github.com>
2023-06-16 20:53:40 -0600

    Code ready for pytest. Problem w/ all_chars

--- pe3.py
+++ pe3.py
@@ -1,38 +1,30 @@
 # 1. Functions
 
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-input_text = input("Enter the text you want to encode: ").lower() # If text is not converted into lower case, the encryption doesn't work
-shift = int(input("Please enter the shift you want to use to encode the entered text: "))
+import string
+
+all_chars = tuple(string.ascii_letters + string.digits + string.punctuation.replace('\\', '') + ' ') # modified alphabet for all_chars to include all most useful characters. However, I needed to also get rid of \ in string.punctuation to avoid escaping characters
 
 def encode(input_text, shift):
-    encoded_string = ""
+    encoded_chars = []
     for char in input_text:
-        if char in alphabet:
-            encoded_index = (alphabet.find(char) + shift) % 26 #Adding modulo 26 ensures that the shift for the encoded letter stays within range
-            encoded_char = alphabet[encoded_index]
-            encoded_string += encoded_char
+        if char in all_chars:
+            encoded_index = (all_chars.index(char) + shift) % len(all_chars)
+            encoded_char = all_chars[encoded_index]
+            encoded_chars.append(encoded_char)
         else:
-            encoded_string += char
-    return encoded_string
+            encoded_chars.append(char)
+    return encoded_chars, ''.join(encoded_chars)
 
 def decode(encoded_string, shift):
-    decoded_string = ""
+    decoded_chars = []
     for char in encoded_string:
-        if char in alphabet:
-            decoded_index = (alphabet.find(char) - shift) % 26
-            decoded_char = alphabet[decoded_index]
-            decoded_string += decoded_char
+        if char in all_chars:
+            decoded_index = (all_chars.index(char) - shift) % len(all_chars)
+            decoded_char = all_chars[decoded_index]
+            decoded_chars.append(decoded_char)
         else:
-            decoded_string += char
-    return decoded_string
-
-encoded_text = encode(input_text, shift)
-
-print("The list of letters encoded are: " + str(tuple(encoded_text)) + " The encoded text is: " + str(encoded_text))
-
-decoded_text = decode(encoded_text, shift)
-
-print("The list of letters decoded are: " + str(tuple(decoded_text)) + " The decoded text is: " + str(decoded_text))
+            decoded_chars.append(char)
+    return decoded_chars, ''.join(decoded_chars)
 
 #2 Classes
 
@@ -75,43 +67,3 @@ class CheckingAccount(BankAccount):
         else:
             super().withdraw(amount)
 
-
-# Test to see if BankAccount class works
-
-test = BankAccount(name="test", ID="321", creation_date=date(2023, 6, 9), balance=0) # BankAccount class works
-
-print(test.name, test.ID, test.creation_date, test.balance)
-
-# First test to see if overdraft works in CheckingAccount
-
-test1 = CheckingAccount(name="test1", ID="321", creation_date=date(2023, 6, 9), balance=0) # Overdraft +$30
-
-print("Balance before withdrawal: ", test1.view_balance())
-
-test1.withdraw(25)
-
-print("Updated balance: ", test1.view_balance())
-
-# Second test to see if we can make a withdrawal from a SavingsAccount that is older than 6 months
-
-test2 = SavingsAccount(name="test2", ID="321", creation_date=datetime.date(2023, 1, 1), balance=100) # Account maturity older than 6 months 
-
-print(test2.name, test2.ID, test2.creation_date, test2.balance)
-
-test2.withdraw(25)
-
-print("Updated balance: ", test2.view_balance())
-
-# Third test to see if we can make a withdrawal from a SavingsAccount that is younger than 6 months
-
-test3 = SavingsAccount(name="test3", ID="321", creation_date=datetime.date.today(), balance=100)
-
-print(test3.name, test3.ID, test3.creation_date, test3.balance)
-
-test3.withdraw(25)
-
-print("Updated balance: ", test3.view_balance())
-
-# Fourth test to see if we can create an account with a future date for creation_date 
-
-test4 = BankAccount(name="test4", ID="321", creation_date=date(2024, 9, 9), balance=50) # Future date raises Exception
