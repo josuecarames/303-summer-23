a939ee1908709afbc26bfccb31d2dc4c052207c1
Josue Carames <35548140+josuecarames@users.noreply.github.com>
2023-08-13 19:30:21 -0600

    Use approx for fixing imprecission issues

--- Group A/problem3_func.py
+++ Group A/problem3_func.py
@@ -4,10 +4,13 @@ def convert_units(amount, us_unit, metric_unit):
 	
 	if not isinstance(us_unit, str) or not isinstance(metric_unit, str):
 		raise Exception("US unit and metric unit must be strings.")
-		
+	
+	if us_unit == metric_unit:
+		return amount
+	
 	conversion_rates = {
-		("pound", "kilogram"): 0.45,
-		("mile", "kilometer"): 1.61,
+		("pound", "kilogram"): 0.453592,
+		("mile", "kilometer"): 1.60934,
 		("fahrenheit", "celsius"): lambda f: 5/9*(f-32),
 		("gallon", "liter"): 3.79
 	}
@@ -16,7 +19,4 @@ def convert_units(amount, us_unit, metric_unit):
 		conversion = conversion_rates[(us_unit, metric_unit)]
 		return conversion(amount) if callable(conversion) else conversion * amount
 	except KeyError:
-		raise Exception("Invalid conversion requested.")
-
-
-
+		raise Exception(f"Invalid conversion requested from {us_unit} to {metric_unit}.")
--- Group A/problem3_tests.py
+++ Group A/problem3_tests.py
@@ -1,10 +1,39 @@
+# import pytest
+# from problem3_func import convert_units 
+# 
+# def test_convert_units():
+# 	assert convert_units(4, "pound","kilogram") == 1.81
+# 	assert convert_units(2, "mile", "kilometer") == 3.22
+# 	assert convert_units(32, "fahrenheit", "celsius") == 0
+# 	assert convert_units(2, "gallon", "liter") == 7.58
+
+# Updated version: Separated tests in multiple functions. Used parametrization to minimize code. Tests have higher coverage.
+
 import pytest
 from problem3_func import convert_units 
+from pytest import approx
 
-def test_convert_units():
-	assert convert_units(4, "pound","kilogram") == 1.8
-	assert convert_units(2, "mile", "kilometer") == 3.22
-	assert convert_units(32, "fahrenheit", "celsius") == 0
-	assert convert_units(2, "gallon", "liter") == 7.58
+@pytest.mark.parametrize("value, from_unit, to_unit, expected", [
+	(4, "pound", "kilogram", 1.81),
+])
+def test_convert_weight(value, from_unit, to_unit, expected):
+	assert convert_units(value, from_unit, to_unit) == approx(expected, 0.01)
 
+@pytest.mark.parametrize("value, from_unit, to_unit, expected", [
+	(2, "mile", "kilometer", 3.22),
+])
+def test_convert_distance(value, from_unit, to_unit, expected):
+	assert convert_units(value, from_unit, to_unit) == approx(expected, 0.01)
+
+@pytest.mark.parametrize("value, from_unit, to_unit, expected", [
+	(32, "fahrenheit", "celsius", 0),
+])
+def test_convert_temperature(value, from_unit, to_unit, expected):
+	assert convert_units(value, from_unit, to_unit) == approx(expected, 0.01)
+
+@pytest.mark.parametrize("value, from_unit, to_unit, expected", [
+	(2, "gallon", "liter", 7.58),
+])
+def test_convert_volume(value, from_unit, to_unit, expected):
+	assert convert_units(value, from_unit, to_unit) == approx(expected, 0.01)
 
